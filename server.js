e181aaf0c278dd476aa57d1c993204799ad437d6
swarajlaha <swaraj.srj@gmail.com>
2020-03-28 20:59:10 +0530

    access user typed info in form

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ let express = require('express')
 
 let app = express()
 
+// tell express add all form values to body object and then add that body object the req object.
+app.use(express.urlencoded({extended: false}))
+
 // if it receives an incoming GET req to the home page url.
 app.get('/', function(req, res) {
     res.send(`<!DOCTYPE html>
@@ -24,9 +27,10 @@ app.get('/', function(req, res) {
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form>
+        <!-- from action -->
+          <form action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
@@ -60,6 +64,12 @@ app.get('/', function(req, res) {
       
     </body>
     </html>`)
+})
+
+// when web browser sends a POST req to the '/create-item' url, in reponse, run the function.
+app.post('/create-item', function(req, res) {
+    console.log(req.body.item)
+    res.send("Thanks for submitting the form")
 })
 
 // tell app to listen for incoming requests, port:3000.
