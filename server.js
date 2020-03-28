185a7c61244cb99b06e3958b5fd0fb127f44ffca
swarajlaha <swaraj.srj@gmail.com>
2020-03-28 22:19:51 +0530

    show data in ui

--- server.js
+++ server.js
@@ -28,56 +28,47 @@ app.use(express.urlencoded({extended: false}))
 
 // if it receives an incoming GET req to the home page url.
 app.get('/', function(req, res) {
-    res.send(`<!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Simple To-Do App</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container">
-        <h1 class="display-4 text-center py-1">To-Do App</h1>
+    db.collection('items').find().toArray(function(err, items) {
+        res.send(`<!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Simple To-Do App</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        </head>
+        <body>
+          <div class="container">
+            <h1 class="display-4 text-center py-1">To-Do App</h1>
+
+            <div class="jumbotron p-3 shadow-sm">
+            <!-- from action -->
+              <form action="/create-item" method="POST">
+                <div class="d-flex align-items-center">
+                  <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                  <button class="btn btn-primary">Add New Item</button>
+                </div>
+              </form>
+            </div>
+
+            <ul class="list-group pb-5">
+              ${items.map(function(item) {
+                  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                  <span class="item-text">${item.text}</span>
+                  <div>
+                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                  </div>
+                </li>`
+              }).join('')}
+            </ul>
+
+          </div>
         
-        <div class="jumbotron p-3 shadow-sm">
-        <!-- from action -->
-          <form action="/create-item" method="POST">
-            <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-              <button class="btn btn-primary">Add New Item</button>
-            </div>
-          </form>
-        </div>
-        
-        <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-        </ul>
-        
-      </div>
-      
-    </body>
-    </html>`)
+        </body>
+        </html>`)
+    })
+    
 })
 
 // when web browser sends a POST req to the '/create-item' url, in reponse, run the function.
