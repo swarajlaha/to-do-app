30698dcdbc4fd5b19960039ca926b265111ac0df
swarajlaha <swaraj.srj@gmail.com>
2020-03-29 11:37:11 +0530

    update on db

--- public/browser.js
+++ public/browser.js
@@ -6,7 +6,7 @@ document.addEventListener("click", function(e) {
         // send on-the-fly POST req to server.
         // post(<url that we want to send a POST req to>, <JS obj-the data that'll be sent along to the url>).
         // 'axios.post' - returns a Promise(used when we're not sure how long an action is going to take).
-        axios.post('/update-item', {text: userInput}).then(function() {
+        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
             // do something
         }).catch(function() {
             console.log("Please try again later.")
--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function(req, res) {
                   return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                   <span class="item-text">${item.text}</span>
                   <div>
-                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
                     <button class="delete-me btn btn-danger btn-sm">Delete</button>
                   </div>
                 </li>`
@@ -88,6 +88,10 @@ app.post('/create-item', function(req, res) {
 
 // send user upated value to 'browser.js'.
 app.post('/update-item', function(req, res) {
-    console.log(req.body.text)
-    res.send("Success")
+    
+    // connect with db.
+    // 'findOneAndUpdate(<which doc to update>, <what to update>, <function that gets called, when db action is complete>)'.
+    db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+        res.send("Success")
+    })
 })
