904e35441a31750f59ba32f50cada913d489f154
swarajlaha <swaraj.srj@gmail.com>
2020-03-29 11:50:44 +0530

    modify for cancel button

--- public/browser.js
+++ public/browser.js
@@ -6,14 +6,17 @@ document.addEventListener("click", function(e) {
         // send on-the-fly POST req to server.
         // post(<url that we want to send a POST req to>, <JS obj-the data that'll be sent along to the url>).
         // 'axios.post' - returns a Promise(used when we're not sure how long an action is going to take).
-        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
+        // IF 'userInput' is NOT blank, ONLY then edit; don't take NULL.
+        if(userInput) {
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
 
-            // this body will run, once the axios req is complete.
-            // manipulate html interface.
-            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
-
-        }).catch(function() {
-            console.log("Please try again later.")
-        })
+                // this body will run, once the axios req is complete.
+                // manipulate html interface.
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+    
+            }).catch(function() {
+                console.log("Please try again later.")
+            })
+        }
     }
 })
