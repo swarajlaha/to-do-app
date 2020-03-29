71b9e2e494cb54c48ba7f4d33098e8c689a76b17
swarajlaha <swaraj.srj@gmail.com>
2020-03-29 13:06:36 +0530

    client side rendering

--- public/browser.js
+++ public/browser.js
@@ -9,6 +9,13 @@ function itemTemplate(item) {
   </li>`
 }
 
+// initial page load render.
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item)
+}).join('')
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
+
 // create feature.
 let createField = document.getElementById("create-field")
 document.getElementById("create-form").addEventListener("submit", function(e) {
