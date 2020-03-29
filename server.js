46ecb7f7d1e1171469efced67f8fd1796c0f9de3
swarajlaha <swaraj.srj@gmail.com>
2020-03-29 13:01:37 +0530

    remove ul items html code

--- server.js
+++ server.js
@@ -57,19 +57,11 @@ app.get('/', function(req, res) {
               </form>
             </div>
 
-            <ul id="item-list" class="list-group pb-5">
-              ${items.map(function(item) {
-                  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-                  <span class="item-text">${item.text}</span>
-                  <div>
-                    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-                  </div>
-                </li>`
-              }).join('')}
-            </ul>
-
+            <ul id="item-list" class="list-group pb-5"></ul>
           </div>
+          <script>
+            let items = ${JSON.stringify(items)} 
+          </script>
           <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
           <script src="/browser.js"></script>
         </body>
